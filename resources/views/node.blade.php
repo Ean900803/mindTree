d97c0ad39cb5568a0fad2261e85497e0a5971b42
Ean <jr00631517@gmail.com>
2024-11-04 17:39:55 +0800

    Revert "直版1"
    This reverts commit b952948164f953e4de48615b6fdd21aeb60dcca6.

--- resources/views/node.blade.php
+++ resources/views/node.blade.php
@@ -1,4 +1,4 @@
-<li class="tree">
+<li class="tree flex flex-col items-center relative pl-10">
     <div class="bg-white shadow rounded-xl p-2 mb-2 text-center h-16 w-40 sm:w-32 relative" id="node-{{$node->id}}">
         {{ $node->value }}<br>
         <button @click="toggleForm()" class="absolute bottom-0 left-1/2 transform -translate-x-1/2 rounded-xl text-gray-400 bg-slate-200 hover:text-gray-600 hover:bg-slate-400">
@@ -16,7 +16,7 @@
         </button>
     </div>
     @if ($node->childNode->isNotEmpty())
-        <ul class="child-container flex flex-row justify-center space-x-4 sm:space-x-2 relative p-1">
+        <ul class="child-container flex flex-row justify-center space-x-4 sm:space-x-2 relative p-5">
             @foreach ($node->childNode as $child)
                 @include('node', ['node' => $child])
             @endforeach
